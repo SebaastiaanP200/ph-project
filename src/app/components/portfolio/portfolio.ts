c814bf4e21137e65942319b51ffdd81aea3ccfa6
Sebastián Pérez <sebastiaan.peerez@gmail.com>
2026-06-02 16:27:48 -0300

    angular-nuevo

--- src/app/components/portfolio/portfolio.ts
+++ src/app/components/portfolio/portfolio.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { NavBar } from '../../shared/components/nav-bar/nav-bar';
-import { Main } from '../../shared/components/main/main';
 import { Footer } from '../../shared/components/footer/footer';
 import { PageTitle } from '../../shared/components/page-title/page-title';
 
--- src/app/components/portfolio/preview-image/preview-image.ts
+++ src/app/components/portfolio/preview-image/preview-image.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-preview-image',
   imports: [],
   templateUrl: './preview-image.html',
   styleUrl: './preview-image.scss',
 })
-export class PreviewImage {}
+export class PreviewImage {
+
+  showPreview: boolean = true;
+
+  changeView() :boolean {
+    this.showPreview = !this.showPreview;
+    return this.showPreview;
+  }
+
+  
+
+
+
+
+
+
+
+}
