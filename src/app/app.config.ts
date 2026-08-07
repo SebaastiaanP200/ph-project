1ed1ac86c0d192aabfdd6d7f316672aff8fba3b6
Sebastián Pérez <sebastiaan.peerez@gmail.com>
2026-08-07 16:20:30 -0300

    angular-nuevo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../enviroments/enviroment';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAFfKGJiQqocXdOr_XJfH6x3WTe9qWi7DI",
+    authDomain: "vc-web-site.firebaseapp.com",
+    projectId: "vc-web-site",
+    storageBucket: "vc-web-site.firebasestorage.app",
+    messagingSenderId: "799751423357",
+    appId: "1:799751423357:web:a1aef7196f4b1508ad3814",
+    measurementId: "G-MF64J25RWN"
+  }
+};
